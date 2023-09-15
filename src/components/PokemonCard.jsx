66165e5e054823d9eaeba9070b1f50182a465ae8
No3ml3 <noemie.barre24@gmail.com>
2023-09-15 15:56:27 +0200

    compoment 2 done wow

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,24 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  let pokemon = pokemonList[0];
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur picture"
-      />
-      <figcaption>bulbasaur</figcaption>
+      {pokemon.imgSrc === undefined ? (
+        <p>???</p>
+      ) : (
+        <img src={pokemon.imgSrc} alt={pokemon.name + " picture"} />
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
