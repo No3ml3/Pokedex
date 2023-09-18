b015fc178d4b69791605721518fd76e0c63c9cf3
No3ml3 <noemie.barre24@gmail.com>
2023-09-18 09:32:17 +0200

    props2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg"; */
 /* import "./App.css"; */
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -11,15 +12,47 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+  const [count, setCount] = useState(0);
+  const beforeButton = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+  const afterButton = () => {
+    if (count < pokemonList.length - 1) {
+      setCount(count + 1);
+    }
+  };
   return (
     <>
       <div>
-        <PokemonCard pokemon={pokemonList[0]}/>
+        <PokemonCard pokemon={pokemonList[count]} />
+        {count > 0 ? <button onClick={beforeButton}>before</button> : ""}
+        {count < pokemonList.length - 1 ? (
+          <button onClick={afterButton}>After</button>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
